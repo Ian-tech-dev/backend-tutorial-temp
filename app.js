708bc3c753217dcaa0b66b538de0a8a492fb8da9
Ian-tech-dev <ringworld21@gmail.com>
2024-12-11 07:04:15 +0300

    event emitter

--- app.js
+++ app.js
@@ -1,46 +1,9 @@
-const {readFile,writeFile} = require('fs').promises
-// const util = require('util')
-// const readFilePromise = util.promisify(readFile)
-// const writeFilePromise = util.promisify(writeFile)
+const http = require('http')
 
-const start = async()=>{
-    try{
-        const first = await readFile('./content/first.txt','utf-8');
-        const second = await readFile('./content/second.txt','utf-8');
-        await writeFile('./content/result-mind-grenade.txt',`This is awesome: 
-            ${first} ${second}`,{flag:'a'})
-        console.log(first,second)
-    }
-    catch(err){
-        console.log(err);
-    }
-    
-}
+const server = http.createServer()
 
-start();
+server.on('request',(req,res)=>{
+    res.end("Welcome");
+})
 
-
-
-// const getText = (path)=>{
-//     return new Promise((resolve,reject)=>{
-//         readFile(path,'utf-8',(err,data)=>{
-//             if(err){
-//                 reject(err);
-//             }else{
-//                 resolve(data);
-//             }
-//         })
-//     })
-// }
-
-
-// getText('./content/first.txt')
-// .then(result=>console.log(result))
-// .catch(err=>console.log(err))
-
-
-
-
-
-
- 
+server.listen(5000)
